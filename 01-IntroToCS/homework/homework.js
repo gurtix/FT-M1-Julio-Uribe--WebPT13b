e1d2a1f4808d2ae99c24df3dabd5942e084c53ad
gurtix <gurtix@hotmail.com>
2023-04-11 17:55:31 -0500

    el mensaje que ustedes quieran

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,30 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var decimal = 0;
+   var longitud = num.length;
+   for (var i = longitud - 1; i >= 0; i--) {
+      
+      if (num.charAt(i) === '1') {
+        decimal = decimal + Math.pow(2, longitud - 1 - i);
+      }
+    }
+    
+    return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   if (num === 0) {
+      return '0';
+    }
+    let binario = '';
+    while (num > 0) {
+      binario = (num % 2) + binario;
+      num = Math.floor(num / 2);
+    }
+    return binario;
+
+}
 
 module.exports = {
    BinarioADecimal,
